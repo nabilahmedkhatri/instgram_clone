14923837c2bcdcacab36c0bb2324a7a91f595a57
Nabil <nabil@admins-macbook-pro.local>
2018-10-24 20:55:42 -0400

    Initial Commit

---
+++ instagram_clone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram_clone
+//
+//  Created by Nabil on 10/24/18.
+//  Copyright © 2018 Nabil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
